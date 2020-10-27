d1b7d7b39343d7468bb452695e7ff7b578f033f1
brect <b.limas93@gmail.com>
2020-10-26 22:28:01 -0300

    Initial commit

---
+++ src/main/java/com/manoloscorp/livinother/LivinotherApplication.java
@@ -0,0 +1,13 @@
+package com.manoloscorp.livinother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivinotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivinotherApplication.class, args);
+    }
+
+}
